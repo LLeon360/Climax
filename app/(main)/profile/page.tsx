ebae39d8f637c28153cf0586798ea74e65c67ae6
christenx <99223047+christen03@users.noreply.github.com>
2024-04-20 16:32:42 -0700

    changes

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,7 +1,10 @@
 "use client"
 import { doc, getDoc, setDoc, updateDoc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
-import { useUser, useFirestore, useFirestoreDocData } from 'reactfire'
+import { useUser, useFirestore,  } from 'reactfire'
+import ClimaxiPhone from "@/public/ClimaxiPhone.png"
+import Watch from "@/public/Watch.png"
+import Image from "next/image"
 
 const Page = () => {
 
@@ -33,11 +36,22 @@ const Page = () => {
 if(!user){
     return <div>Loading...</div>
 }
-  return (
-    <div>
-      {userCode && <p>Your unique code: {userCode}</p>}
+return (
+  <div className="flex flex-col md:flex-row items-center justify-center p-4">
+    <div className="text-center md:text-left md:mr-8">
+      <h1 className="text-2xl font-bold mb-2">Your code is:</h1>
+      <p className="text-9xl font-semibold mb-4">{userCode}</p>      <div className="text-sm md:text-base">
+        <p>Download the Climax app and enter your code on your phone!</p>
+        <p>Then, join a room and your watch will automatically start tracking your heart rate!</p>
+      </div>
     </div>
-  )
-}
+    <div className="flex flex-col items-center md:items-start">
+      <Image src={Watch} alt="Watch" width={200} height={200} />
+      <Image src={ClimaxiPhone} alt="Climax iPhone" width={200} height={200} className="mt-4" />
+    </div>
+  </div>
+);
+};
+
 
 export default Page
